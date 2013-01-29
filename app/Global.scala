e11043d2664a7225b5b08dd87ca0f6b1ffc0ab14
Josh Monson <camman3d@gmail.com>
2013-01-29 15:13:55 -0700

    Added additional logging for all errors.

--- app/Global.scala
+++ app/Global.scala
@@ -1,6 +1,12 @@
 import play.api._
+import mvc.RequestHeader
 
 object Global extends GlobalSettings {
+
+  override def onError(request: RequestHeader, ex: Throwable) = {
+    Logger.error("Play Error: " + ex.getMessage)
+    play.api.mvc.Results.BadRequest
+  }
 
   override def onStart(app: Application) {
     createFixtures()
