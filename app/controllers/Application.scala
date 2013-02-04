f3012bceb76bf41367cedea50521bf661f66379a
Josh Monson <camman3d@gmail.com>
2013-02-04 13:33:40 -0700

    Added phonetics tutor question extractor

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,8 +3,10 @@ package controllers
 import play.api.mvc._
 import tools.{Grader, OAuthTools}
 import com.google.gdata.client.authn.oauth._
-import models.Problem
+import models.{User, Problem}
 import anorm.NotAssigned
+import play.api.libs.ws.WS
+import play.api.libs.json.{JsValue, JsArray}
 
 object Application extends Controller {
 
@@ -13,15 +15,83 @@ object Application extends Controller {
       Ok(views.html.index())
   }
 
-  def test = Action {
+  def test = TODO
+
+  def extract(username: String) = Action {
     implicit request =>
 
-      val problem = Problem(NotAssigned, "Test Problem", List("Red", "Yellow", "Blue"), 'multiple, 1, 1, caseModifier = false,
-        punctuationModifier = false, wordOrderModifier = false, responseOrderModifier = true)
-      val responses = List("Yellow", "Blue")
-      val grade = Grader.grade(responses, problem)
+      var user = User.findByUsername(username).get
+      var count = 0
 
-      Ok(grade.grade.toString)
+      // Get all exercise groups
+      val exerciseGroups = WS.url("http://sartre3.byu.edu:5984/phtut_exercisegroup/_all_docs").get().await.get.json \ "rows"
+
+      // For each group
+      for (groupEntry <- exerciseGroups.as[List[JsValue]]) {
+//      val groups = exerciseGroups.as[List[JsValue]]
+//      for (i <- 0 until 10) {
+//        val groupEntry = groups(i)
+
+        // Not a design doc
+        val groupId = (groupEntry \ "id").as[String]
+        if (groupId.substring(0,1) != "_") {
+
+          // Get the group
+          val group = WS.url("http://sartre3.byu.edu:5984/phtut_exercisegroup/" + groupId).get().await.get.json
+          val groupName = (group \ "name").as[String]
+
+          // For each exercise
+          for (exerciseId <- (group \ "exercises").as[List[String]]) {
+
+            // Get the exercise
+            val exercise = WS.url("http://sartre3.byu.edu:5984/phtut_exercise/" + exerciseId).get().await.get.json
+            val exerciseName = (exercise \ "name").as[String]
+            val name = groupName + " -- " + exerciseName
+
+            // For each element
+            for (element <- (exercise \ "elements").as[List[JsValue]]) {
+
+              // If it's a question
+              if ((element \ "elementType").as[String] == "question") {
+
+                // Get the question
+                val questionId = (element \ "value").as[String]
+                val question = WS.url("http://sartre3.byu.edu:5984/phtut_question/" + questionId).get().await.get.json
+
+                // Create the problem from the question
+                user = createProblemFromQuestion(question, name, user)
+                count += 1
+              }
+            }
+          }
+        }
+      }
+
+      user.save
+      Ok(count + " problems created")
+  }
+
+  def createProblemFromQuestion(question: JsValue, name: String, user: User): User = {
+    // Get the answers
+    val answers = (question \ "answer").as[List[String]]
+
+    // Get the problem type
+    val variety = (question \ "variety").as[String]
+    val problemType =
+      if (variety == "dragAndDrop" || variety == "multipleAnswer" || variety == "multipleSelect" || variety == "plusMinus")
+        'multiple
+      else
+        'single
+
+    // Get the acceptance rate and case modifier
+    val acceptanceRate = if (variety == "text" || variety == "textarea") 0.5 else 1.0
+    val caseModifier = if (variety == "text" || variety == "textarea") true else false
+
+    val problem = Problem(NotAssigned, name, answers, problemType, 1, acceptanceRate, caseModifier,
+      punctuationModifier = false, wordOrderModifier = false, responseOrderModifier = true, gradientGradeMethod = false,
+      subtractiveModifier = false, multipleGradeModifier = true)
+
+    user.addFloatingProblem(problem)
   }
 
   def testPost = Action {
